3f99ce4b0ad710efb5bbe937480e8fd04bededbd
Rajeev Kumar Alahari <ra833@njit.edu>
2025-02-25 18:42:18 -0500

    updated code

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,25 @@
-class App:
-    @staticmethod
-    def start() -> None:
-        print("Hello World. Type 'exit' to exit.")
-        
-        while True:
-            user_input = input(">>> ")
-            if user_input.lower() == "exit":
-                print("Exiting...")
-                break
-            else:
-                # Here, you could add additional commands and their handling
-                print("Unknown command. Type 'exit' to exit.")
+from app.commands import CommandHandler
+from app.commands.discord import DiscordCommand
+from app.commands.exit import ExitCommand
+from app.commands.goodbye import GoodbyeCommand
+from app.commands.greet import GreetCommand
+from app.commands.menu import MenuCommand
 
-        
+class App:
+    def __init__(self): # Constructor
+        self.command_handler = CommandHandler()
+
+
+    def start(self):
+        # Register commands here
+        self.command_handler.register_command("greet", GreetCommand())
+        self.command_handler.register_command("goodbye", GoodbyeCommand())
+        self.command_handler.register_command("exit", ExitCommand())
+        self.command_handler.register_command("menu", MenuCommand())
+        self.command_handler.register_command("discord", DiscordCommand())
+
+        print("Type 'exit' to exit.")
+        while True:  #REPL Read, Evaluate, Print, Loop
+            self.command_handler.execute_command(input(">>> ").strip())
+
+
